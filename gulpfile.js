9ec70bcca20aae8d9bd53bc94f71c93b17762305
leiding <leiding12@creditease.cn>
2017-03-01 17:57:57 +0800

    a

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,35 @@
-var gulp=require("gulp");
-
-
-var uglify = require('gulp-uglify');  //加载js压缩
+let  gulp=require("gulp");
+let uglify = require('gulp-uglify');  //加载js压缩
+let babel=require("gulp-babel");
+let jshint=require("gulp-jshint");
+let cssMin = require('gulp-css'); 
+let imagemin = require('gulp-imagemin');
+let runSequence=require("run-sequence");
+let del=require("del");
+gulp.task("clean",function(){ 
+    del(['dist'])
+});
 gulp.task('jsCompress', function () {
     gulp.src(['./assets/js/**/*.js','!./assets/js/abc/exclude.js'])  //获取文件，同时过滤掉.min.js文件
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js/'));  //输出文件
+         .pipe(babel())
+         .pipe(uglify())
+         .pipe(gulp.dest('dist/js/')); //输出文件
 });
 
-
-var jshint=require("gulp-jshint");
 gulp.task("jsHint",function(){ 
-	gulp.src('./assets/js/**/*.js')
-	    .pipe(jshint())
-	    .pipe(jshint.reporter('default'));
-});
+    gulp.src('./assets/js/**/*.js')
+     .pipe(jshint({
+            esversion: 6
+        }))
+    .pipe(jshint.reporter('default'))
+ });
 
-
-var cssMin = require('gulp-css'); 
 gulp.task('cssMinfy', function(){
   return gulp.src('./assets/css/**/*.css')
     .pipe(cssMin())
     .pipe(gulp.dest('dist/css/'));
 });
 
-
-var imagemin = require('gulp-imagemin');
 gulp.task('imageMin', function () {
     return gulp.src('./assets/images/*')
         .pipe(imagemin(
@@ -36,7 +40,15 @@ gulp.task('imageMin', function () {
         ))
         .pipe(gulp.dest('dist/images'));
 });
+gulp.task('build',function(done){ 
+    runSequence(
+        'clean',
+        ['jsCompress', 'cssMinfy', 'imageMin'],
+        done
+    )
+})
 
-gulp.task('default', ['jsHint','jsCompress','cssMinfy','imageMin']);
+
+gulp.task('default', ['build']);
 
 
